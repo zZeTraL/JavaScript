f225fdcd4e763dce4f78a0331e6de93050be9b6b
zZeTraL <lucashbs.pro@gmail.com>
2021-10-18 20:51:15 +0200

    🌠Update

--- TD/TD1/EX5/script.js
+++ TD/TD1/EX5/script.js
@@ -9,6 +9,25 @@ var getRandomNumber = (min = 200, max = 250) => {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
+/**
+ * simulateParty simule plusieurs parties en fonction de n
+ * 
+ * @param {any} n 
+ * @returns {Array}
+ */
+var simulateParty = (n) => {
+    let result = [0, 0];
+    for (let i = 0; i < n; i++) {
+        let tmp = getRandomNumber();
+        result[1] += tmp;
+        for (let j = 0; j < tmp; j++) {
+            let random = Math.random();
+            if(random <= 0.78){result[0]++;}
+        }
+    }
+    return result;
+};
+
 let guest = getRandomNumber();
 let guestWhoDrinks = 0;
 //
@@ -21,16 +40,8 @@ for (let index = 0; index < guest; index++) {
 
 console.log("\nPour une simple soirée...\nNombre de personnes présentent à la soirée: " + guest);
 console.log("Nombre de personnes voulant boire le brevage divin: " + guestWhoDrinks);
-console.log("\nPour 5 soirées...");
+console.log("\nPour 1000 simulation...");
 
-guestWhoDrinks = 0;
-for (let repeat = 0; repeat < 5; repeat++) {
-    for (let index = 0; index < guest; index++) {
-        let tmp = Math.random();
-        if (tmp <= 0.78) {
-            guestWhoDrinks++;
-        }
-    }
-}
-
-console.log("Il faut donc préparer en moyenne: " + Math.ceil(guestWhoDrinks / 5) + " cocktails du brevage divin !");
+let t = simulateParty(1000);
+console.log(t[0], t[1]);
+console.log("En moyenne, il faut préparer: " + (t[0] / 1000) + " cocktails du brevage divin !");
