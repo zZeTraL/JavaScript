107bad6a1e2eb25a3ea8032819f76c2b97a0dcba
zZeTraL <lucashbs.pro@gmail.com>
2021-12-21 01:26:29 +0100

    🌠 Update TP4 - EX1: refactoring some code - EX3: updated css and html

--- TP/TP4/EX1/main.js
+++ TP/TP4/EX1/main.js
@@ -22,6 +22,18 @@
 	observableObject.trigger('move', 'Alice', 2, 1);
 
 	observableObject.off('win', winCallback);
+	console.log(observableObject);
 	observableObject.trigger('win', 'Alice');
 	observableObject.trigger('move', 'Bob', 1, 1);
 })();
+
+// EXEMPLE QUI MA PERMIS DE COMPRENDRE
+/*let array = [['win', 1], ['move', 0], ['win', 1]]
+console.log(array)
+for (let i = 0; i < array.length; i++) {
+    if(array[i].find((str) => str === 'win')){
+        console.log("Win found at index: " + i);
+        array[i].splice(0, 2)
+    }
+}
+console.log(array)*/
