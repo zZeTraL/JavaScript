46fcce83c69921fc455600a5dc362c7c4456bf01
zZeTraL <lucashbs.pro@gmail.com>
2022-01-05 15:59:29 +0100

    🌠 Update - Some enhancement + comments

--- TP/TP4/EX3/js/class/TicTacToeView.js
+++ TP/TP4/EX3/js/class/TicTacToeView.js
@@ -1,45 +1,77 @@
-class TicTacToeView{
+class TicTacToeView {
     constructor(game, name) {
         this.gameCore = game;
         this.gameName = name;
-
         this.displayCurrentPlayer(this.gameCore.getCurrentPlayer());
     }
 
-    // Setters
-    displayCurrentPlayer(number){
+    /*
+     *          SETTERS
+     */
+
+    /**
+     * Permet de mettre à jour une balise html afin d'afficher lequel joueur est actuellement en train de joueur
+     *
+     * @param number
+     */
+    displayCurrentPlayer(number) {
         document.getElementById("player_number").textContent = "C'est au joueur: " + number;
     }
 
-    displaySymbol(column, line, index){
-        if(this.gameCore.getCaseState(column, line) === undefined){
+    /**
+     * Cette fonction permet d'afficher le symbole O ou X grâce à une position donnée
+     *
+     * @param column
+     * @param line
+     * @param index
+     */
+    displaySymbol(column, line, index) {
+        if (this.gameCore.getCaseState(column, line) === undefined) {
             let gridCase = document.getElementsByTagName("td");
             gridCase[index - 1].textContent = this.gameCore.getCurrentPlayer();
         } else {
-            let outputResult = document.getElementById("output");
-            outputResult.textContent = "This case is already crossed !!!";
-            outputResult.classList.add("warning");
-            setTimeout(function(){
-                outputResult.textContent = "";
-                outputResult.classList.remove("warning");
-            }, 1500);
+            this.setOutput("This case is already crossed !!!");
+            document.getElementById("output").classList.add("warning");
+            setTimeout(this.clearOutput, 1500);
         }
     }
 
-    // Methods
-    isGameFinished(){
-        if(this.gameCore.isFinished()){
+    /*
+     *          METHODS
+     */
+
+    /**
+     * Permet de reset l'output
+     */
+    clearOutput(){
+        let outputResult = document.getElementById("output");
+        outputResult.setAttribute("class", "");
+        outputResult.textContent = "";
+    }
+
+    /**
+     * Permet d'afficher un message dans l'output
+     *
+     * @param str
+     */
+    setOutput(str){ document.getElementById("output").textContent = str; }
+
+    /**
+     *  Permet de savoir si la partie est finis et ainsi mettre à jour une balise html
+     *  afin de communiquer le résultat de la partie
+     *
+     * @returns {boolean}
+     */
+    isGameFinished() {
+        if (this.gameCore.isFinished()) {
             let outputResult = document.getElementById("output");
-            //console.log("GAME IS FINISHED, DO WE HAVE A WINNER ???");
-            if(this.gameCore.hasWinner()){
-                //console.log("GAME IS FINISHED, WE HAVE A WINNER !!!!");
-                //console.log("THE WINNER IS: " + this.gameCore.getWinner());
-                //console.log("GAME: " + this.gameName + " IS NOW FINISHED !!!");
-                outputResult.textContent = "GAME IS FINISHED (ID: " + this.gameName + "), DO WE HAVE A WINNER ?\nTHE WINNER IS " + this.gameCore.getWinner();
+            if (this.gameCore.hasWinner()) {
+                this.setOutput("GAME IS FINISHED (ID: " + this.gameName + "), DO WE HAVE A WINNER ?\nTHE WINNER IS " + this.gameCore.getWinner());
+                setTimeout(this.clearOutput, 5000);
                 return true;
             } else {
-                //console.log("NO WINNER :'(");
-                outputResult.textContent = "GAME IS FINISHED (ID: " + this.gameName + "), DO WE HAVE A WINNER ?\nNO WINNER :'(";
+                this.setOutput("GAME IS FINISHED (ID: " + this.gameName + "), DO WE HAVE A WINNER ?\nNO WINNER :'(");
+                setTimeout(this.clearOutput, 5000);
                 return true;
             }
         } else {
@@ -47,7 +79,12 @@ class TicTacToeView{
         }
     }
 
-    reset(){
+
+    /**
+     * Cette fonction permet de recommencer à zéro une partie
+     * Elle s'occupe de clear l'affichage / reset les variables etc...
+     */
+    reset() {
         this.gameCore.reset();
         let gridCase = document.getElementsByTagName("td");
         for (const gridCaseElement of gridCase) {
@@ -56,22 +93,39 @@ class TicTacToeView{
         this.displayCurrentPlayer(0);
     }
 
+    /**
+     *          LISTENER
+     */
 
     /**
-     * Fonction associé au listener qui va permettre de diférencier les cases
+     * Fonction associé au listener qui va permettre de différencier les cases
+     *
      * @param event
      */
-    caseClicked(event){
-        let caseIndex = event.target.getAttribute("data");
-        // DEBUG
-        //console.log("Case index is: " + caseIndex);
+    caseClicked(event) {
 
-        switch (caseIndex){
+        /*                                  E X P L I C A T I O N
+         *
+         *  Chaque case du tableau (dans le html) possède un attribut data unique à celle-ci
+         *  On va donc identifier chaque cas où le data est différent et exécuter des instructions
+         *  Par exemple lorsque je clique sur la case qui est identifié par le code html suivant : <td data="1"></td>
+         *  on remarque que la balise <td> possède comme attribut data="1" :
+         *      1. Notre switch s'occupe de faire une série de test
+         *      2. Ici data="1", on se retrouve dans le premier cas
+         *      3. On va donc exécuter la fonction displaySymbol() qui va AFFICHER soit (O ou X) en fonction du joueur et la case
+         *      4. Bien évidemment on doit venir mettre à jour notre array qui contient l'état de chaque case (gameCore)
+         *         C'est l'objectif de la fonction gameCore.play() qui va changer la valeur à l'index I et J par (toujours) soit O ou X
+         *      5. On va aussi mettre à jour le joueur i.e faire jouer le prochain joueur
+         *      6. On regarde si la partie est terminé (un vainqueur ou non) si c'est le cas on va reset la partie
+         */
+
+        let caseIndex = event.target.getAttribute("data");
+        switch (caseIndex) {
             case "1":
                 this.displaySymbol(0, 0, caseIndex);
                 this.gameCore.play(0, 0);
                 this.displayCurrentPlayer(this.gameCore.getCurrentPlayer());
-                if(this.isGameFinished()){
+                if (this.isGameFinished()) {
                     this.reset();
                     break;
                 }
@@ -81,7 +135,7 @@ class TicTacToeView{
                 this.displaySymbol(1, 0, caseIndex);
                 this.gameCore.play(1, 0);
                 this.displayCurrentPlayer(this.gameCore.getCurrentPlayer());
-                if(this.isGameFinished()){
+                if (this.isGameFinished()) {
                     this.reset();
                     break;
                 }
@@ -91,7 +145,7 @@ class TicTacToeView{
                 this.displaySymbol(2, 0, caseIndex);
                 this.gameCore.play(2, 0);
                 this.displayCurrentPlayer(this.gameCore.getCurrentPlayer());
-                if(this.isGameFinished()){
+                if (this.isGameFinished()) {
                     this.reset();
                     break;
                 }
@@ -101,7 +155,7 @@ class TicTacToeView{
                 this.displaySymbol(0, 1, caseIndex);
                 this.gameCore.play(0, 1);
                 this.displayCurrentPlayer(this.gameCore.getCurrentPlayer());
-                if(this.isGameFinished()){
+                if (this.isGameFinished()) {
                     this.reset();
                     break;
                 }
@@ -111,7 +165,7 @@ class TicTacToeView{
                 this.displaySymbol(1, 1, caseIndex);
                 this.gameCore.play(1, 1);
                 this.displayCurrentPlayer(this.gameCore.getCurrentPlayer());
-                if(this.isGameFinished()){
+                if (this.isGameFinished()) {
                     this.reset();
                     break;
                 }
@@ -121,7 +175,7 @@ class TicTacToeView{
                 this.displaySymbol(2, 1, caseIndex);
                 this.gameCore.play(2, 1);
                 this.displayCurrentPlayer(this.gameCore.getCurrentPlayer());
-                if(this.isGameFinished()){
+                if (this.isGameFinished()) {
                     this.reset();
                     break;
                 }
@@ -131,7 +185,7 @@ class TicTacToeView{
                 this.displaySymbol(0, 2, caseIndex);
                 this.gameCore.play(0, 2);
                 this.displayCurrentPlayer(this.gameCore.getCurrentPlayer());
-                if(this.isGameFinished()){
+                if (this.isGameFinished()) {
                     this.reset();
                     break;
                 }
@@ -141,7 +195,7 @@ class TicTacToeView{
                 this.displaySymbol(1, 2, caseIndex);
                 this.gameCore.play(1, 2);
                 this.displayCurrentPlayer(this.gameCore.getCurrentPlayer());
-                if(this.isGameFinished()){
+                if (this.isGameFinished()) {
                     this.reset();
                     break;
                 }
@@ -151,12 +205,11 @@ class TicTacToeView{
                 this.displaySymbol(2, 2, caseIndex);
                 this.gameCore.play(2, 2);
                 this.displayCurrentPlayer(this.gameCore.getCurrentPlayer());
-                if(this.isGameFinished()){
+                if (this.isGameFinished()) {
                     this.reset();
                     break;
                 }
                 break;
-
             default:
                 break;
         }
